18cd6ab11a0defcd06ed6a435721872541a96bb2
Jean-Pierre Gattuso <gattuso@obs-vlfr.fr>
2020-05-27 04:21:46 +0000

    Added scripts from Jim Orr to investigate the uncertaintie in pK1 and pK2

---
+++ prettyaxes.R
@@ -0,0 +1,4 @@
+ box()
+ axis(1,labels=FALSE, tcl=-0.35) ; axis(1, lwd=0, line=-0.4, labels=TRUE)
+ axis(2,labels=FALSE,tcl=-0.35)  ; axis(2, lwd=0, line=-0.4, las=2, labels=TRUE)
+ axis(3,labels=FALSE,tcl=-0.35) ; axis(4,labels=FALSE,tcl=-0.25)
